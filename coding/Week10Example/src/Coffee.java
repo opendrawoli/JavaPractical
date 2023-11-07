931d236ab85aef51d6d13155cea328af31dccde8
Opendra <opendraoli66@gmail.com>
2023-11-07 10:47:52 -0500

    Creating Coffee class

---
+++ coding/Week10Example/src/Coffee.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author OPend
+ *
+ */
+public class Coffee {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
